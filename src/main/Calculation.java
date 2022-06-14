86f291df0a0479a005521de6ad89be8ac6e577a5
niludarekar1 <nilu.darekar1@gmail.com>
2022-06-14 10:26:39 +0530

    Added: Cab Invoice Genarator Main Demo

---
+++ src/main/Calculation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Calculation {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int cube(int x) {
+        return (x*x*x);
+    }
+}
